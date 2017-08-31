521a4b03798d77c30cf668986f33c321a97713b1
Roy Scheffers <r.scheffers@gmail.com>
2017-08-31 17:52:55 +0200

    update default page template and sidebar

--- page.php
+++ page.php
@@ -1,21 +1,31 @@
 <?php get_header(); ?>
-	
-	<div id="content">
-	
-		<div id="inner-content" class="row">
-	
-		    <main id="main" class="large-8 medium-8 columns" role="main">
-				
-				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
+<style>
+	.wide-screen {width: 100vw; height: 500px;}
+	.page-title {color: white;}
+	.entry-content {background-color: white;}
+	#main {position:relative; top:-250px;}
+</style>
+
+	<div id="content">
+		<div class="header-image">
+			<?php the_post_thumbnail( 'large', ['class' => 'wide-screen'] ); ?>
+		</div>
+		<div id="inner-content" class="row expanded">
+			<div class="sidebar">
+				<?php get_sidebar(); ?>
+			</div>
+
+		    <main id="main" class="large-7 medium-7 small-12 columns end" role="main">
+
+				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 			    	<?php get_template_part( 'parts/loop', 'page' ); ?>
-			    
-			    <?php endwhile; endif; ?>							
-			    					
+			    <?php endwhile; endif; ?>
+
 			</main> <!-- end #main -->
 
-		    <?php get_sidebar(); ?>
-		    
+
+
 		</div> <!-- end #inner-content -->
 
 	</div> <!-- end #content -->
