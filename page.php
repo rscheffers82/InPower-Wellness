ef35e8a1c0cc3bedf3a9a1b31f15b17d66576782
Roy Scheffers <r.scheffers@gmail.com>
2017-09-01 16:17:26 +0200

    improve default page template

--- page.php
+++ page.php
@@ -2,9 +2,10 @@
 
 <style>
 	.wide-screen {width: 100vw; height: 500px;}
-	.page-title {color: white;}
 	.entry-content {background-color: white;}
-	#main {position:relative; top:-250px;}
+	.page-title {color:black;}
+	.featured-image {position:relative; top:-250px;}
+	.featured-image .page-title {color: white;}
 </style>
 
 	<div id="content">
@@ -16,7 +17,12 @@
 				<?php get_sidebar(); ?>
 			</div>
 
-		    <main id="main" class="large-7 medium-7 small-12 columns end" role="main">
+				<?php global $post;
+				if (has_post_thumbnail($post->ID)): ?>
+		    	<main id="main" class="large-7 medium-7 small-12 columns end featured-image" role="main">
+				<?php else: ?>
+		    	<main id="main" class="large-7 medium-7 small-12 columns end" role="main">
+				<?php endif ?>
 
 				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 			    	<?php get_template_part( 'parts/loop', 'page' ); ?>
