c7155bc329690b57a1695c6105055858a2585f37
AndrewSepic <andrew@thinkupdesign.ca>
2017-09-15 13:07:51 -0400

    More styles and SVG test

--- template-home.php
+++ template-home.php
@@ -28,6 +28,19 @@ Template Name: Home
 					<?php the_field('hero_button_description'); ?>
 				</a>
 			</div>
+
+		<svg width="1440px" height="105px" viewBox="0 0 1440 105" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+		    <!-- Generator: Sketch 46.1 (44463) - http://www.bohemiancoding.com/sketch -->
+		    <desc>Created with Sketch.</desc>
+		    <defs></defs>
+		    <g id="Homepage" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+		        <g id="Desktop-HD" transform="translate(0.000000, -559.000000)" fill="#FFFFFF">
+		            <g id="Hero-Section" transform="translate(-23.000000, 121.000000)">
+		                <path d="M20,543 C256.648257,473 494.454496,438 733.418717,438 C972.382938,438 1215.5767,473 1463,543 L20,543 Z" id="Arc"></path>
+		            </g>
+		        </g>
+		    </g>
+		</svg>
 		</div>
 
 		<div id="inner-content" class="row">
