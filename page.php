6451ce4ad6c6a9285a035a1f4956dc75a40136a0
AndrewSepic <andrew@thinkupdesign.ca>
2017-09-25 16:13:26 -0400

    changes to header image, removing Roys inline styles and merging into main.sass

--- page.php
+++ page.php
@@ -1,30 +1,20 @@
 <?php get_header(); ?>
 
-<style>
-	.wide-screen {width: 100vw; height: 500px;}
-	.entry-content {background-color: white;}
-	.page-title {color:black;}
-	.featured-image {position:relative; top:-250px;}
-	.featured-image .page-title {color: white;}
-</style>
-
 	<div id="content">
-		<div class="header-image">
-			<?php the_post_thumbnail( 'large', ['class' => 'wide-screen'] ); ?>
+		<div class="header-image" style="background-image: url('<?php the_post_thumbnail( 'large', ['class' => 'wide-screen'] ); ?>')">
+			<svg class="arc" viewBox="0 0 1440 105" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+			    <!-- Generator: Sketch 46.1 (44463) - http://www.bohemiancoding.com/sketch -->
+			    <desc>Created with Sketch.</desc>
+			    <defs></defs>
+			    <g id="Homepage" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+			        <g id="Desktop-HD" transform="translate(0.000000, -559.000000)" fill="#FFFFFF">
+			            <g id="Hero-Section" transform="translate(-23.000000, 121.000000)">
+			                <path d="M20,543 C256.648257,473 494.454496,438 733.418717,438 C972.382938,438 1215.5767,473 1463,543 L20,543 Z" id="Arc"></path>
+			            </g>
+			        </g>
+			    </g>
+			</svg>
 		</div>
-		
-		<svg class="arc" viewBox="0 0 1440 105" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-		    <!-- Generator: Sketch 46.1 (44463) - http://www.bohemiancoding.com/sketch -->
-		    <desc>Created with Sketch.</desc>
-		    <defs></defs>
-		    <g id="Homepage" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-		        <g id="Desktop-HD" transform="translate(0.000000, -559.000000)" fill="#FFFFFF">
-		            <g id="Hero-Section" transform="translate(-23.000000, 121.000000)">
-		                <path d="M20,543 C256.648257,473 494.454496,438 733.418717,438 C972.382938,438 1215.5767,473 1463,543 L20,543 Z" id="Arc"></path>
-		            </g>
-		        </g>
-		    </g>
-		</svg>
 
 		<div id="inner-content" class="row expanded">
 			<div class="sidebar">
