f4ab4a5e3cacca2c0d9314caba500c0489a4eaa8
AndrewSepic <andrew@thinkupdesign.ca>
2017-09-25 16:38:31 -0400

    resetting sidebar.php to default

--- sidebar.php
+++ sidebar.php
@@ -1,20 +1,16 @@
 <div id="sidebar1" class="sidebar large-4 medium-4 columns" role="complementary">
 
-<?php thinkup_testimonials(); ?>
+	<!-- Initial code from the template -->
+	<?php if ( is_active_sidebar( 'sidebar1' ) ) : ?>
 
+		<?php dynamic_sidebar( 'sidebar1' ); ?>
+
+	<?php else : ?>
+
+	<!-- This content shows up if there are no widgets defined in the backend. -->
+	<div class="alert help">
+		<p><?php _e( 'Please activate some Widgets.', 'jointswp' );  ?></p>
+	</div>
+
+	<?php endif; ?>
 </div>
-
-<!-- Initial code from the template -->
-<?php //if ( is_active_sidebar( 'sidebar1' ) ) : ?>
-
-	<?php //dynamic_sidebar( 'sidebar1' ); ?>
-
-<?php //else : ?>
-
-<!-- This content shows up if there are no widgets defined in the backend. -->
-
-<!-- <div class="alert help"> -->
-	<p><?php //_e( 'Please activate some Widgets.', 'jointswp' );  ?></p>
-<!-- </div> -->
-
-<?php //endif; ?>
