3101d2f9e2964f8901f79165258f7f65fb4c08c3
AndrewSepic <andrew@thinkupdesign.ca>
2017-10-02 16:54:20 -0400

    Added markup to blog index.php

--- index.php
+++ index.php
@@ -1,28 +1,42 @@
 <?php get_header(); ?>
-			
+
 	<div id="content">
-	
+		<div class="header-image" style="background-image: url('wp-content/themes/inpower/assets/images/articles.jpg')">
+			<svg class="arc" viewBox="0 0 1440 105" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+			    <!-- Generator: Sketch 46.1 (44463) - http://www.bohemiancoding.com/sketch -->
+			    <desc>Created with Sketch.</desc>
+			    <defs></defs>
+			    <g id="Homepage" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+			        <g id="Desktop-HD" transform="translate(0.000000, -559.000000)" fill="#FFFFFF">
+			            <g id="Hero-Section" transform="translate(-23.000000, 121.000000)">
+			                <path d="M20,543 C256.648257,473 494.454496,438 733.418717,438 C972.382938,438 1215.5767,473 1463,543 L20,543 Z" id="Arc"></path>
+			            </g>
+			        </g>
+			    </g>
+			</svg>
+		</div>
+
 		<div id="inner-content" class="row">
-	
+
 		    <main id="main" class="large-8 medium-8 columns" role="main">
-		    
+
 			    <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-			 
+
 					<!-- To see additional archive styles, visit the /parts directory -->
 					<?php get_template_part( 'parts/loop', 'archive' ); ?>
-				    
-				<?php endwhile; ?>	
+
+				<?php endwhile; ?>
 
 					<?php joints_page_navi(); ?>
-					
+
 				<?php else : ?>
-											
+
 					<?php get_template_part( 'parts/content', 'missing' ); ?>
-						
+
 				<?php endif; ?>
-																								
+
 		    </main> <!-- end #main -->
-		    
+
 		    <?php get_sidebar(); ?>
 
 		</div> <!-- end #inner-content -->
