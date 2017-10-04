6884a35fbf21ee93d7d6881ae9dd9280a40a15bf
AndrewSepic <andrew@thinkupdesign.ca>
2017-10-04 15:55:09 -0400

    adding psuedo elements to blog roll for orange cover

--- parts/loop-archive.php
+++ parts/loop-archive.php
@@ -1,7 +1,9 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(''); ?> role="article">
 	<div class="row">
 		<div class="large-4 medium-4 columns">
-			<a href="<?php the_permalink() ?>"><?php the_post_thumbnail('blog-thumb'); ?></a>
+			<div class="thumb-wrap">
+				<a href="<?php the_permalink() ?>"><?php the_post_thumbnail('blog-thumb'); ?></a>
+			</div>
 		</div>
 		<div class="large-8 medium-8 columns">
 			<header class="article-header">
