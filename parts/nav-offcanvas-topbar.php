400f5adf6f6192ab549cd504ecc28c79aa2ed6bc
AndrewSepic <andrew@thinkupdesign.ca>
2017-10-05 16:29:50 -0400

    modifying layout in mobile menu & more styles for responsive nav

--- parts/nav-offcanvas-topbar.php
+++ parts/nav-offcanvas-topbar.php
@@ -2,6 +2,12 @@
 	 and a topbar for medium-up -->
 
 <div class="top-bar" id="top-bar-menu">
+	<div class="top-bar-right float-right hide-for-medium">
+		<ul class="menu">
+			<li><button class="menu-icon" type="button" data-toggle="off-canvas"></button></li>
+			<li><a data-toggle="off-canvas"><?php _e( 'Menu', 'jointswp' ); ?></a></li>
+		</ul>
+	</div>
 	<div class="top-bar-left float-left">
 		<ul class="menu">
 			<li class="logo"><a href="<?php echo home_url(); ?>"><img src="<?php echo get_template_directory_uri() . '/assets/images/inpower-logo.svg'; ?>" alt="InPower Wellness Logo"/></a></li>
@@ -9,11 +15,5 @@
 	</div>
 	<div class="top-bar-right show-for-medium">
 		<?php joints_top_nav(); ?>
-	</div>
-	<div class="top-bar-right float-right hide-for-medium">
-		<ul class="menu">
-			<!-- <li><button class="menu-icon" type="button" data-toggle="off-canvas"></button></li> -->
-			<li><a data-toggle="off-canvas"><?php _e( 'Menu', 'jointswp' ); ?></a></li>
-		</ul>
 	</div>
 </div>
